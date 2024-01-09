3ef18a775fbfefe8f3f123f93e558ae835d35b8b
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-01-09 17:52:22 +0530

    extract view

--- SwiftUI-1/ContentView.swift
+++ SwiftUI-1/ContentView.swift
@@ -12,21 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         List(books) { book in
-            HStack(alignment: .top) {
-                Image(uiImage: .add)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 50)
-                VStack(alignment: .leading) {
-                    Text(book.title)
-                        .font(.headline)
-                    Text(book.author)
-                        .font(.subheadline)
-                    Text(book.isbn)
-                        .font(.subheadline)
-                }
-                Spacer()
-            }
+            BookRowView(book: book)
         }
     }
 }
@@ -34,5 +20,27 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(books: Book.sampleBooks)
+    }
+}
+
+struct BookRowView: View {
+    var book: Book
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            Image(uiImage: .add)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 50)
+            VStack(alignment: .leading) {
+                Text(book.title)
+                    .font(.headline)
+                Text(book.author)
+                    .font(.subheadline)
+                Text(book.isbn)
+                    .font(.subheadline)
+            }
+            Spacer()
+        }
     }
 }
