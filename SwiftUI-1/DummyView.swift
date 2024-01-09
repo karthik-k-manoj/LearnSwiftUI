a19dadbc1929cec16ced0012e27547017bf3aa1b
Karthik K Manoj <dotz10@gmail.com>
2024-01-09 15:21:45 +0530

    update gitignore

---
+++ SwiftUI-1/DummyView.swift
@@ -0,0 +1,20 @@
+//
+//  DummyView.swift
+//  SwiftUI-1
+//
+//  Created by Karthik K Manoj on 09/01/24.
+//
+
+import SwiftUI
+
+struct DummyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DummyView_Previews: PreviewProvider {
+    static var previews: some View {
+        DummyView()
+    }
+}
