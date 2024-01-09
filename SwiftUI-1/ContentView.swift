31321ef022df46e743545795c1197d07afeaec9e
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-01-09 16:24:23 +0530

    put text views inside v stack.
    put image and v stack inside H stack with a spacer

--- SwiftUI-1/ContentView.swift
+++ SwiftUI-1/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Asynchronous Programming With SwiftUI and Combine")
-        .font(.headline)
+        HStack(alignment: .top) {
+            Image(uiImage: .add)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 50)
+            VStack(alignment: .leading) {
+                Text("Asynchronous Programming With SwiftUI and Combine")
+                    .font(.headline)
+                Text("by Peter Friese")
+                    .font(.subheadline)
+                Text("451 pages")
+                    .font(.subheadline)
+            }
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(.sizeThatFits)
     }
 }
