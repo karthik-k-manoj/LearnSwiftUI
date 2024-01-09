ec2cb9f7b609b71e62d71e09baf0bc357faf1f7c
Karthik K Manoj <dotz10@gmail.com>
2024-01-09 14:59:02 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple swiftUI learning project 
---
+++ SwiftUI-1/SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_1App.swift
+//  SwiftUI-1
+//
+//  Created by Karthik K Manoj on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
