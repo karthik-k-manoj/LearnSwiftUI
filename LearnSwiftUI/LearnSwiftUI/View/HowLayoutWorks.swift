f5b6de3371e3c8ad1c8f9ee86868e366dfc11db5
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-10 16:42:40 +0530

    add howlayoutworksview

---
+++ LearnSwiftUI/LearnSwiftUI/View/HowLayoutWorks.swift
@@ -0,0 +1,20 @@
+//
+//  ViewSize.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 10/07/24.
+//
+
+import SwiftUI
+
+struct HowLayoutWorks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewSize_Previews: PreviewProvider {
+    static var previews: some View {
+        HowLayoutWorks()
+    }
+}
