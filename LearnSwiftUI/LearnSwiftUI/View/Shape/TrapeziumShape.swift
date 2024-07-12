c986625d443bd7cfc2af9999659ef0979c5113cd
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-12 17:00:38 +0530

    add new file

---
+++ LearnSwiftUI/LearnSwiftUI/View/Shape/TrapeziumShape.swift
@@ -0,0 +1,20 @@
+//
+//  TrapeziumShape.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 12/07/24.
+//
+
+import SwiftUI
+
+struct TrapeziumShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrapeziumShape_Previews: PreviewProvider {
+    static var previews: some View {
+        TrapeziumShape()
+    }
+}
