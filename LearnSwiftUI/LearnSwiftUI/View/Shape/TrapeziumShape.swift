4cff4b7057ef946b07fdc96d49722b5dd2aa7fc7
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-13 11:36:29 +0530

    use UIRectCorner to tell shape which corner point to offset

--- LearnSwiftUI/LearnSwiftUI/View/Shape/TrapeziumShape.swift
+++ LearnSwiftUI/LearnSwiftUI/View/Shape/TrapeziumShape.swift
@@ -14,15 +14,19 @@ import SwiftUI
  with in reference to this frame size.
  */
 struct TrapeziumShape: Shape {
+    var offset: CGFloat = 0.5
+    var corner: UIRectCorner = .bottomLeft
     // rect is reported size for a shape view in which the path will be drawn
     // This is only required method for a shape
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
+        // Start point
         path.move(to: .zero)
-        path.addLine(to: CGPoint(x: rect.maxX, y: 0))
+        // Top left
+        path.addLine(to: CGPoint(x: rect.maxX, y: corner == .topLeft ? rect.maxY * offset :  0))
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: 0.5 * rect.height))
+        path.addLine(to: CGPoint(x: 0, y: corner == .bottomLeft ? offset * rect.height : rect.maxY))
         path.closeSubpath()
         
         return path
@@ -33,18 +37,35 @@ struct TrapeziumShape_Previews: PreviewProvider {
     static var previews: some View {
         let image = Image("myImage")
         
-        image
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-            .border(Color.red)
-            .frame(height: 200)
-            .clipShape(TrapeziumShape())
-            .overlay(content: {
-                TrapeziumShape()
-                    .stroke(Color.black, lineWidth: 2)
-            })
-            .border(Color.blue)
-            .padding(10)
+        VStack {
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .border(Color.red)
+                .frame(height: 200)
+                .clipShape(TrapeziumShape())
+                .overlay(content: {
+                    TrapeziumShape(offset: 0.5, corner: .bottomLeft)
+                        .stroke(Color.black, lineWidth: 1)
+                })
+                .border(Color.blue)
+                .padding(10)
+            
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .border(Color.red)
+                .frame(height: 200)
+                .clipShape(TrapeziumShape(offset: 0.5, corner: .topLeft))
+                .overlay(content: {
+                    TrapeziumShape(offset: 0.5, corner: .topLeft)
+                        .stroke(Color.black, lineWidth: 2)
+                })
+                .border(Color.blue)
+                .padding(10)
+                
+        }
+       
     }
 }
 
