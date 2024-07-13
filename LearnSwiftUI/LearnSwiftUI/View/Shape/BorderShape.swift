f2315aeda04cf78edf6c034988fce31ea3f4a1bf
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-13 12:44:22 +0530

    draw border shape path and stroke it

--- LearnSwiftUI/LearnSwiftUI/View/Shape/BorderShape.swift
+++ LearnSwiftUI/LearnSwiftUI/View/Shape/BorderShape.swift
@@ -9,12 +9,30 @@ import SwiftUI
 
 struct BorderShape: Shape {
     func path(in rect: CGRect) -> Path {
-        Path()
+        Path { path in
+            path.move(to: rect[.topLeading])
+            path.addLine(to: rect[.topTrailing])
+            path.addLine(to: rect[.bottomTrailing])
+            path.addLine(to: rect[.init(x: 0.5, y: 0.8)])
+            path.addLine(to: rect[.bottomLeading])
+            path.addLine(to: rect[.topLeading])
+            path.closeSubpath()
+        }
+
     }
 }
 
 struct BorderShape_Previews: PreviewProvider {
     static var previews: some View {
-        BorderShape()
+      BorderShape()
+            .stroke(Color.black)
+            .frame(width: 80, height: 200)
+    }
+}
+
+extension CGRect {
+    subscript(_ point: UnitPoint) -> CGPoint {
+        CGPoint(x: minX + width * point.x, y: minY + height * point.y)
+        
     }
 }
