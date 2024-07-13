7d084f43adf586d6cf188921e56b15dff37bd409
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-13 12:05:32 +0530

    add new file

---
+++ LearnSwiftUI/LearnSwiftUI/View/Shape/BorderShape.swift
@@ -0,0 +1,20 @@
+//
+//  BorderShape.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 13/07/24.
+//
+
+import SwiftUI
+
+struct BorderShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path()
+    }
+}
+
+struct BorderShape_Previews: PreviewProvider {
+    static var previews: some View {
+        BorderShape()
+    }
+}
