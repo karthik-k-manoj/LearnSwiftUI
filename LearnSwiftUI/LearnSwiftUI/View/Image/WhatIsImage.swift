d3c825a7f6aaf587a7131bd0fab950eb80fee08f
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-15 11:47:00 +0530

    understand  image

--- LearnSwiftUI/LearnSwiftUI/View/Image/WhatIsImage.swift
+++ LearnSwiftUI/LearnSwiftUI/View/Image/WhatIsImage.swift
@@ -19,16 +19,21 @@ import SwiftUI
 
 struct WhatIsImage: View {
     var body: some View {
-        // leaf view which doesn't have a subview
-       Image("myImage")
-            .resizable()
-            // A parent (modifier) can ask it's child multiple times before it returns from here.
-            // So Aspect Ratio will ask size by giving nil x nil to get the it's ideal size
-            // then do some modification to and proposes new size which the view might accept and returns.
-            // Aspect Ratio will return the modifed size
-            // The parent is asking aspect ratio what is your size? Aspect ratio has a child. It consults with the child to get the it's size
-            // and that is the size of the aspect ratio.
-            .aspectRatio(contentMode: .fit)
+        VStack {
+            Image("myImage")
+                 // A parent (modifier) can ask it's child multiple times before it returns from here.
+                 // So Aspect Ratio will ask size by giving nil x nil to get the it's ideal size
+                 // then do some modification to and proposes new size which the view might accept and returns.
+                 // Aspect Ratio will return the modifed size
+                 // The parent is asking aspect ratio what is your size? Aspect ratio has a child. It consults with the child to get the it's size
+//                 // and that is the size of the aspect ratio.
+                 .aspectRatio(contentMode: .fit)
+            Image("myImage")
+                .resizable(capInsets: EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0), resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 500, height: 500)
+            
+        }
     }
 }
 
