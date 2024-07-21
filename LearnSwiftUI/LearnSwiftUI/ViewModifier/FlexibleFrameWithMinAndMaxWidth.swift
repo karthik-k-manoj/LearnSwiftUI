720498e95dcf1e69e427f94a42863a54866e7e5d
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-21 11:07:25 +0530

    give more clarity

--- LearnSwiftUI/LearnSwiftUI/ViewModifier/FlexibleFrameWithMinAndMaxWidth.swift
+++ LearnSwiftUI/LearnSwiftUI/ViewModifier/FlexibleFrameWithMinAndMaxWidth.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct FlexibleFrameWithMinAndMaxWidth: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Here the text will be it's own size but you can see frame is what is proposed to it")
+            .fixedSize()
             .frame(minWidth: 0, maxWidth: .infinity)
             .border(Color.red)
             .frame(width: 200)
@@ -17,7 +18,7 @@ struct FlexibleFrameWithMinAndMaxWidth: View {
         // when this kind of frame is set with min and max
         // then this means its size will be the proposed size
         // but height is from the child view even though
-        // we propose a larger height 
+        // we propose a larger height
     }
 }
 
