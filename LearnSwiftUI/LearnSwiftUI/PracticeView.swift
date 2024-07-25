09d3af8b6f5241228cd7567ac7e5a34d632620ea
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-25 20:15:22 +0530

    understanding flexible frame when passed nil as the proposed size

--- LearnSwiftUI/LearnSwiftUI/PracticeView.swift
+++ LearnSwiftUI/LearnSwiftUI/PracticeView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct PracticeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // scroll view proposes nil as it's height to i'ts children
+        ScrollView {
+            Color.red
+            // when the proposed size is `nil` then ideal height is used to propose to child, and that becomes min height to clamp
+            // say child say I need to be mininum of 450 then this frame
+            // will be 450 height
+                .frame(idealHeight: 400, maxHeight: 500)
+                .debugLog("Inside  Frame")
+        }
+        .border(.black)
+        .debugLog("Scroll View")
     }
 }
 
