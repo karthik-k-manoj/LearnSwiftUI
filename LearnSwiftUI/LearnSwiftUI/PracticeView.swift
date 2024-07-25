d7c249afc6f4727bb5ee0adf8acdd02af19576a2
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-25 18:32:15 +0530

    add practice view

---
+++ LearnSwiftUI/LearnSwiftUI/PracticeView.swift
@@ -0,0 +1,20 @@
+//
+//  PracticeView.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 25/07/24.
+//
+
+import SwiftUI
+
+struct PracticeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PracticeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PracticeView()
+    }
+}
