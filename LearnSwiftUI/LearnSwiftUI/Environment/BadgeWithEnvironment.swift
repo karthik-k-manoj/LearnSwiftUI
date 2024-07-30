7f62e73b1a4f1a837b57316e6b42997a778aab58
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-30 10:50:20 +0530

    apply tint colour environment modifier

--- LearnSwiftUI/LearnSwiftUI/Environment/BadgeWithEnvironment.swift
+++ LearnSwiftUI/LearnSwiftUI/Environment/BadgeWithEnvironment.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct BadgeWithEnvironment: View {
     var body: some View {
-        Text("3000")
-            .modifier(Badge())
-            .tint(.red)
-        
+        VStack {
+            Text("3000")
+                .modifier(Badge())
+            Text("3000")
+                .modifier(Badge())
+        }
+        .tint(.red)
+        // badge will now render with using current tint color
+            // falling back to the sysrem default if we don't
+            // specify a color. For e.g. we can change the tint color to red for entiree subtree
     }
 }
 
