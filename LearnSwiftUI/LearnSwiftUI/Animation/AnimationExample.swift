eaabc2110c89acef018e2095c62360a56fffc98b
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-02 13:50:00 +0530

    explicit animation using `withAnimation`

---
+++ LearnSwiftUI/LearnSwiftUI/Animation/AnimationExample.swift
@@ -0,0 +1,31 @@
+//
+//  AnimationExample.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 02/08/24.
+//
+
+import SwiftUI
+
+struct AnimationExample: View {
+    @State private var scaleUp: CGFloat = 1
+    
+    var body: some View {
+        Button {
+            // this reutrns result of recomputing a view's body with the provided animation
+            withAnimation(.interpolatingSpring(stiffness: 60, damping: 2)) {
+                scaleUp *= 1.4
+            }
+        } label: {
+            Text("Scale up")
+                .scaleEffect(scaleUp)
+        }
+
+    }
+}
+
+struct AnimationExample_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationExample()
+    }
+}
