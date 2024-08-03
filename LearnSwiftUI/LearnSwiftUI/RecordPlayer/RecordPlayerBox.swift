987fa59f9f5e17620b048c2b7e083646d485001d
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-03 20:31:50 +0530

    add record player box file

---
+++ LearnSwiftUI/LearnSwiftUI/RecordPlayer/RecordPlayerBox.swift
@@ -0,0 +1,20 @@
+//
+//  RecordPlayerBox.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 03/08/24.
+//
+
+import SwiftUI
+
+struct RecordPlayerBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordPlayerBoxV_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordPlayerBox()
+    }
+}
