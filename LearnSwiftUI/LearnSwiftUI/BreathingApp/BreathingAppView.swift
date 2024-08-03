437acb60fda15bcaf2b8a86cd8e63bbb4cee3aef
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-03 15:18:35 +0530

    animate circles in pair by rotating scaling and move in and out

--- LearnSwiftUI/LearnSwiftUI/BreathingApp/BreathingAppView.swift
+++ LearnSwiftUI/LearnSwiftUI/BreathingApp/BreathingAppView.swift
@@ -10,13 +10,82 @@ import SwiftUI
 struct BreathingAppView: View {
     @State var scaleUpDown = false
     @State var rotateInOut = false
-    @State var moveInOut = false
+    @State var moveInOut = true
     
     var body: some View {
         ZStack {
-         Rectangle()
-                .foregroundColor(Color.black)
-                .edgesIgnoringSafeArea(.all)
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [.green, .white]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ))
+                    .frame(width: 120, height: 120)
+                    .offset(y: moveInOut ? -60 : 0)
+                Circle()
+                    .fill(LinearGradient(
+                        gradient: Gradient(colors: [.green, .white]),
+                        startPoint: .bottom,
+                        endPoint: .top
+                    ))
+                    .frame(width: 120, height: 120)
+                    .offset(y: moveInOut ? 60 : 0)
+            }
+            .opacity(0.5)
+            
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [.green, .white]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ))
+                    .frame(width: 120, height: 120)
+                    .offset(y: moveInOut ? -60 : 0)
+                Circle()
+                    .fill(LinearGradient(
+                        gradient: Gradient(colors: [.green, .white]),
+                        startPoint: .bottom,
+                        endPoint: .top
+                    ))
+                    .frame(width: 120, height: 120)
+                    .offset(y: moveInOut ? 60 : 0)
+            }
+            .opacity(0.5)
+            .rotationEffect(.degrees(60))
+            
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [.green, .white]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ))
+                    .frame(width: 120, height: 120)
+                    .offset(y: moveInOut ? -60 : 0)
+                Circle()
+                    .fill(LinearGradient(
+                        gradient: Gradient(colors: [.green, .white]),
+                        startPoint: .bottom,
+                        endPoint: .top
+                    ))
+                    .frame(width: 120, height: 120)
+                    .offset(y: moveInOut ? 60 : 0)
+            }
+            .opacity(0.5)
+            .rotationEffect(.degrees(120))
+        }
+        .rotationEffect(.degrees(rotateInOut ? 90 : 0))
+        .scaleEffect(scaleUpDown ? 1/4 : 1)
+        .animation(.easeInOut.repeatForever().speed(1/8), value: scaleUpDown)
+        .onAppear {
+            rotateInOut.toggle()
+            scaleUpDown.toggle()
+            moveInOut.toggle()
         }
     }
 }
