c5d9d49a6a5e65efe6ea1a1e66cf40295c25b65e
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-03 21:35:24 +0530

    add `audioPlayer`

---
+++ LearnSwiftUI/LearnSwiftUI/RecordPlayer/PlaySound.swift
@@ -0,0 +1,24 @@
+//
+//  PlaySound.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 03/08/24.
+//
+
+import AVFoundation
+
+final class AudioPlayer {
+    private var audioPlayer: AVAudioPlayer?
+
+    func playSound(sound: String, type: String) {
+        if let path = Bundle.main.path(forResource: sound, ofType: type) {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+                audioPlayer?.play()
+            } catch {
+                print("Could not find and play the sound file")
+            }
+        }
+    }
+}
+
