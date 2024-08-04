9d42519fb85dd90030a9f950114dbec35a20c1f9
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-04 19:36:09 +0530

    animation modifier value can be called as animation variable. Whenever it changes then animate modifier will animate any change in the state of the view subtree which are animatable

--- LearnSwiftUI/LearnSwiftUI/Animation/NonViewSubtreeAnimatingCheck.swift
+++ LearnSwiftUI/LearnSwiftUI/Animation/NonViewSubtreeAnimatingCheck.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 struct NonViewSubtreeAnimatingCheck: View {
     @State private var isWidthChanged = false
+    @State private var isHeightChanged = false
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .background(.red)
-            .animation(.default, value: isWidthChanged)
-            .frame(width: isWidthChanged ? 200 : 100, height: 50)
+            .frame(width: isWidthChanged ? 200 : 100, height: isHeightChanged ? 100 : 50)
             .background(.yellow)
+            // value is `iswidthChanged` that is what the animation modifier is observing at
+            // if that changed from the rendered tree then it is going to animate whatever below the animation modifier to the new state. For e.g here we changed height as well
+            .animation(.default, value: isWidthChanged)
             .onTapGesture {
                 isWidthChanged.toggle()
+                isHeightChanged.toggle()
             }
         
         // the frame is not animating because frame modifier is not under animation modifier. Need to check in later iOS 17
@@ -26,5 +31,6 @@ struct NonViewSubtreeAnimatingCheck: View {
 struct NonViewSubtreeAnimatingCheck_Previews: PreviewProvider {
     static var previews: some View {
         NonViewSubtreeAnimatingCheck()
+            .previewLayout(.sizeThatFits)
     }
 }
