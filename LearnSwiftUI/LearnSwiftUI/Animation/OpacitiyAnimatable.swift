8cf2abbdc0f7949148932a939f337aff8333308b
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-06 11:38:34 +0530

    add new file

---
+++ LearnSwiftUI/LearnSwiftUI/Animation/OpacitiyAnimatable.swift
@@ -0,0 +1,21 @@
+//
+//  OpacitiyAnimatable.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 06/08/24.
+//
+
+import SwiftUI
+
+struct OpacitiyAnimatable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OpacitiyAnimatable_Previews: PreviewProvider {
+    static var previews: some View {
+        OpacitiyAnimatable()
+    }
+}
+
