c927c4cb2dbf6ac76d05d9dd91db238dc1902824
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-18 13:56:56 +0530

    withAnimation method executes the closure and then recomputes the view body with transaction at the top of the view tree is returned

---
+++ LearnSwiftUI/LearnSwiftUI/Animation/AnimationDemo/WithAnimation.swift
@@ -0,0 +1,41 @@
+//
+//  WithAnimation.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 18/08/24.
+//
+
+import SwiftUI
+
+struct WithAnimation: View {
+    @State var red = false
+    @State var x: CGFloat = 0
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Circle()
+                .fill(red ? .red : .blue)
+                .frame(width: 30, height: 30)
+                .offset(x: x)
+            Spacer()
+            Button("Animate") {
+                if x == 0 {
+                    x = 100
+                } else {
+                    x = 0
+                }
+                
+                withAnimation(.easeIn(duration: 1)) {
+                    red.toggle()
+                }
+            }
+        }
+        .frame(width: 500, height: 300)
+        .border(.red)
+    }
+}
+
+#Preview {
+    WithAnimation()
+}
