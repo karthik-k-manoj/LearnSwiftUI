599f831df03164777fe72a1f3ba43b11b039ca7d
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-18 15:17:05 +0530

    animation witthou value does not work with associating multiple dependecies of the same animatable wdiget

---
+++ LearnSwiftUI/LearnSwiftUI/Animation/AnimationDemo/AnimatableWithSpecificDependency.swift
@@ -0,0 +1,44 @@
+//
+//  AnimatableWithSpecificDependency.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 18/08/24.
+//
+
+import SwiftUI
+
+struct AnimatableWithSpecificDependency: View {
+    @State var x: CGFloat = 0
+    @State var y: CGFloat = 0
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Circle()
+                .fill(.orange)
+                .frame(width: 30, height: 30)
+                .offset(x: x, y: y)
+                .animation(.spring, value: x)
+                .animation(.linear, value: y)
+            Spacer()
+            Button("Animate") {
+                if x == 0 {
+                    x = 100
+                } else {
+                    x = 0
+                }
+                
+                if y == 0 { y = 100 } else {
+                    y = 0
+                }
+            }
+        }
+        .frame(width: 500, height: 500)
+    }
+}
+/*
+ Currently animation<V>(_ animation: Animation?, value: V) does not support associating different time curve function with different dependencies of the same animatable component
+ */
+#Preview {
+    AnimatableWithSpecificDependency()
+}
