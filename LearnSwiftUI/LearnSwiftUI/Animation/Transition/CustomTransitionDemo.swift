3c4932c015363feef32da884fba8fc1e09dba026
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-22 12:39:04 +0530

    define MyTransition

---
+++ LearnSwiftUI/LearnSwiftUI/Animation/Transition/CustomTransitionDemo.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTransitionDemo.swift
+//  LearnSwiftUI
+//
+//  Created by Karthik K Manoj on 22/08/24.
+//
+
+import SwiftUI
+
+struct CustomTransitionDemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTransitionDemo()
+}
