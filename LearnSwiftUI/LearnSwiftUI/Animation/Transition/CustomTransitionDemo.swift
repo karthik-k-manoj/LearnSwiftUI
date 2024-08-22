ef95f554e21620fc18103d171488c5b38f4817b6
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-22 12:39:33 +0530

    add new file and define mytransiiton view modiifer

--- LearnSwiftUI/LearnSwiftUI/Animation/Transition/CustomTransitionDemo.swift
+++ LearnSwiftUI/LearnSwiftUI/Animation/Transition/CustomTransitionDemo.swift
@@ -7,6 +7,15 @@
 
 import SwiftUI
 
+struct MyTransition: ViewModifier {
+    let rotation: Angle
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(rotation) // animatable component
+    }
+}
+
 struct CustomTransitionDemo: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
