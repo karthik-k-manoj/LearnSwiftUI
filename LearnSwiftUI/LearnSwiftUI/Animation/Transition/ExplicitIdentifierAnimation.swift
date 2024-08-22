edb663b726d688437601c12be9fee1e5ccd45e3f
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-08-22 15:38:46 +0530

    explicit id of for each will identiy the system to animate transition

--- LearnSwiftUI/LearnSwiftUI/Animation/Transition/ExplicitIdentifierAnimation.swift
+++ LearnSwiftUI/LearnSwiftUI/Animation/Transition/ExplicitIdentifierAnimation.swift
@@ -7,14 +7,20 @@
 
 import SwiftUI
 
+struct Item: Identifiable, Equatable {
+    let id = UUID() // unique identifier
+    let number: Int
+}
+
+
 struct ExplicitIdentifierAnimation: View {
-    @State private var items = (0...100).map { $0 }
+    @State private var items = (0...100).map { Item(number: $0) }
     
     var body: some View {
         VStack {
             List {
-                ForEach(items, id: \.self) { item in
-                    Text("\(item)")
+                ForEach(items, id: \.id) { item in
+                    Text("\(item.number)")
                 }
             }
         }
@@ -26,7 +32,7 @@ struct ExplicitIdentifierAnimation: View {
         // if duplicate elements are added to list then it causes issues so we need to identify them
         
         Button("Add second") {
-            items.insert(Int.random(in: 0...100), at: 1)
+            items.insert(Item(number: Int.random(in: 0...100)), at: 1)
         }
     }
 }
