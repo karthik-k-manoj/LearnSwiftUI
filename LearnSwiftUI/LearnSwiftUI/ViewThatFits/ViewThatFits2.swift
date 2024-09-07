6b10dc5bc9dd2fa6c18efe95efdf753c91f0dac1
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-09-07 12:56:39 +0530

    understanding fixed size modifier axis vs axiss of VTF

--- LearnSwiftUI/LearnSwiftUI/ViewThatFits/ViewThatFits2.swift
+++ LearnSwiftUI/LearnSwiftUI/ViewThatFits/ViewThatFits2.swift
@@ -48,6 +48,12 @@ struct ViewThatFits2_1: View {
     }
 }
 
+
+// This constraint of ideal size on a single axis
+// corresponds exactly to the restricted axes setting in
+// VFT initializer.
+
+
 struct IdealSizeText: View {
     var body: some View {
         Text("GeometryReader has been present since the birth of SwiftUI, playing a crucial role in many scenarios.")
@@ -58,14 +64,46 @@ struct IdealSizeText: View {
     }
 }
 
-// This constraint of ideal size on a single axis
-// corresponds exactly to the restricted axes setting in
-// VFT initializer. Through config we can make VFT only
-// evaluate child view's ideals izes on certain axes
+/*
+ Now understand there is a relationship between fixedSize (proposing nil x nil) with the constrainted axes inside `ViewThatFits` initializer
+ Through config we can make VFT only
+ evaluate child view's ideals izes on certain axes
+ 
+ Below VTF says I need to only consider `.vertical` axis. This means
+ To decide which view to be selected VTF proposed width X nil for each view
+ So depending on this combination text shrinks as there is a width and the height of the text will go beyond the proposed height of VTF parent.
+ 
+ Developers must be clear that VFT makes judgement based on ideal sizes but the selected child view is not rendered in it's ideal state during the display. So if the proposed size is less then subview has to to truncate the text according to the default display rules during final rendering
+ 
+ */
 
-
+struct IdealSizeText3: View {
+    var body: some View {
+        HStack {
+            ViewThatFits(in: .vertical) {
+                Text("1: GeometryReader has been present since the birth of SwiftUI, playing a crucial role in many scenarios.")
+                                Text("2: In addition, some views believe that:")
+            }
+            .border(.blue)
+            .frame(width: 200, height: 100, alignment: .top)
+            .border(.red)
+            
+            Text("1: GeometryReader has been present since the birth of SwiftUI, playing a crucial role in many scenarios.")
+                .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                .border(.blue)
+                .frame(width: 200, height: 100, alignment: .top)
+                .border(.red)
+            
+            Text("2: In addition, some views believe that:")
+                .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                .border(.blue)
+                .frame(width: 200, height: 100, alignment: .top)
+                .border(.red)
+        }
+    }
+}
 
 
 #Preview {
-    IdealSizeText()
+    IdealSizeText3()
 }
