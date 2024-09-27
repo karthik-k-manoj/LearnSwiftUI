e36fa3a5ceeb190e2c2d7bf6d5363de273a08561
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-09-27 11:03:31 +0530

    create collection view by mimic ForEach struct API

--- LearnSwiftUI/LearnSwiftUI/Preference/CollectionViewFlowLayoutUsingPreference.swift
+++ LearnSwiftUI/LearnSwiftUI/Preference/CollectionViewFlowLayoutUsingPreference.swift
@@ -7,20 +7,35 @@
 
 import SwiftUI
 
+struct CollectionView<Elements, Content>: View where Elements: RandomAccessCollection, Content: View, Elements.Element: Identifiable {
+    var data: Elements
+    var content: (Elements.Element) -> Content
+    
+    var body: some View {
+        HStack {
+            ForEach(data) { string in
+                self.content(string)
+            }
+        }
+    }
+}
+
+extension String: @retroactive Identifiable {
+    public var id: String { self }
+}
+
 struct CollectionViewFlowLayoutUsingPreference: View {
     var strings = (1...10).map {
         "Item \($0)" + String(repeatElement("x", count: Int.random(in: 0...10)))
     }
     
     var body: some View {
-        HStack {
-            ForEach(strings, id: \.self) { string in
-                Text(string)
-                    .padding(10)
-                    .background {
-                        Color.red
-                    }
-            }
+        CollectionView(data: strings) { string in
+            Text(string)
+                .padding(10)
+                .background {
+                    Color.red
+                }
         }
     }
 }
